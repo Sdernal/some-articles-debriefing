84bceb6d84eb267d700ab8b5ec4d5f976d3e1f91
Владислав Корзун <sdernal2@gmail.com>
2025-10-18 19:20:32 +0300

    Debugging Tilted model

---
+++ src/models/tri_plane_model.py
@@ -0,0 +1,11 @@
+from torch import nn
+
+
+class TriPlaneNetwork(nn.Module):
+    # TODO: Finish with Tri-plane model
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def forward(self):
+        pass
